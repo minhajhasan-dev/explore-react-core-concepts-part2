cdf32f31673a62d878820fe3b9b54287f6ddc831
minhaj.dev <minhajhasan.dev@gmail.com>
2024-03-13 07:45:41 +0600

    explore event, state, useEffect

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Friends from "./Friends";
+import Users from "./Users";
+import Counter from "./counter";
+import Team from "./team";
+
+function App() {
+  return (
+    <>
+      <h3>React Core Concepts 2</h3>
+      <Friends></Friends>
+      <Users></Users>
+      <Team></Team>
+      <Counter></Counter>
+    </>
+  );
+}
+
+export default App;
